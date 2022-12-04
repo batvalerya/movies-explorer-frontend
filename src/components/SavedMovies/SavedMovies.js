4232c4497c8fde7db9176026e3bceebae083777d
Baturina Valeriya <bat.valerya@yandex.ru>
2022-12-04 19:04:01 +0700

    Preloader

--- src/components/SavedMovies/SavedMovies.js
+++ src/components/SavedMovies/SavedMovies.js
@@ -1,6 +1,7 @@
 import MoviesCardList from "../MoviesCardList/MoviesCardList.js"
 import {savedMovies} from "../../utils/consts.js"
 import Header from "../Header/Header.js";
+import Footer from "../Footer/Footer.js";
 
 function SavedMovies() {
     return(
@@ -9,6 +10,7 @@ function SavedMovies() {
             <section className="saved-movies-card-list">
                 <MoviesCardList moviesCards={savedMovies} />
             </section>
+            <Footer />
         </>
     )
 };
