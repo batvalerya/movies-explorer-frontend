ee8bb4b8dc9795fc848487c87ce47661c8f3388a
Baturina Valeriya <bat.valerya@yandex.ru>
2022-12-05 00:05:17 +0700

    добавить кнопку удаления для сохраненных фильмов

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,10 +1,19 @@
+import { useLocation } from 'react-router-dom';
+
 function MoviesCard({movieCard}) {
+
+    const location = useLocation();
+    const savedMovies = location.pathname === '/saved-movies';
+
     return(
         <li className="movies-card">
             <div className="movie-card__info">
                 <h3 className="movies-card__title">{movieCard.nameRu}</h3>
                 <p className="movies-card__duration">{movieCard.duration}</p>
-                <button className="movies-card__save-button" type="button" />
+                <button
+                className={`movies-card__button ${savedMovies ? 'movies-card__delete-button' : 'movies-card__save-button'}`}
+                    type="button" 
+                />
             </div>
             <img className="movies-card__img" src={movieCard.image} alt={movieCard.nameRu} />
         </li>
