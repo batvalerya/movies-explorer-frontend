14e2775736099904d5089254a35a884223e2570d
Baturina Valeriya <bat.valerya@yandex.ru>
2022-12-04 14:12:02 +0700

    NotFoundPage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import Header from '../Header/Header.js';
-// import Main from '../Main/Main.js';
-// import Footer from '../Footer/Footer.js';
-// import Movies from '../Movies/Movies.js';
+import Main from '../Main/Main.js';
+import Footer from '../Footer/Footer.js';
+import Movies from '../Movies/Movies.js';
 import Profile from '../Profile/Profile.js';
 import { user } from '../../utils/consts.js';
 import Register from '../Register/Register.js';
+import NotFound from '../NotFound/NotFound.js'
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
       {/* <Movies /> */}
       {/* <Profile userInfo={user}/> */}
       {/* <Register /> */}
-      <Register />
+      {/* <Register /> */}
+      <NotFound />
     </div>
   )
 };
---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,17 @@
+function NotFound() {
+    return(
+        <div className="not-found-page">
+            <h1 className="not-found-page__title">
+                404
+            </h1>
+            <p className="not-found-page__subtitle">
+                Страница не найдена
+            </p>
+            <button className="not-found-page__btn">
+                Назад
+            </button>
+        </div>
+    )
+};
+
+export default NotFound;
